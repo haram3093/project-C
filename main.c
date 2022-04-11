0a010f33b113672aa2fcbea66c5458c6e9ec1ae1
DragonTerrorKing <95440305+DragonTerrorKing@users.noreply.github.com>
2022-04-11 14:33:22 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -61,29 +61,110 @@ void display(int ** tab, int TMAX){
     printf("\n");
 }
 
-int info_case(int ** tab, int x, int y){
-    return tab[x][y];
+int info_case(int ** tab, int TMAX, int x, int y){
+    if(y < 0 || x < 0 || (x < 0 && y < 0) || y > TMAX - 1 || x > TMAX - 1 || (x > TMAX - 1 && y > TMAX - 1))
+    {
+       return 1;
+    }
+    else
+    {
+        return tab[x][y];
+    }
 }
 
-// void remplirTableau(int ** tab){
-//     int r = 0;
-//     int c = 0;
-//     for(r = 0; r < 5; r++)
-//         for(c = 0; c < 5; c++)
-//             tab[r][c] = 100;
+
+// int pierre_isoler(int ** tab, int x, int y)
+// {
+// 	int piece = info_case(tab, x, y);
+// 	int up = info_case(tab, x - 1, y);
+// 	int down = info_case(tab, x + 1, y);
+// 	int right = info_case(tab, x, y + 1);
+// 	int left = info_case(tab, x, y - 1);
+// 	int liberter = 0;
+// 	if(up == 0)
+// 	{
+// 		liberter += 1;
+// 	}
+// 	if(down == 0)
+// 	{
+// 		liberter += 1;
+// 	}
+// 	if(right == 0)
+// 	{
+// 		liberter += 1;
+// 	}
+// 	if(left == 0)
+// 	{
+// 		liberter += 1;
+// 	}
+	
+// 	if (piece == 1)
+// 	{
+// 		printf("La pierre Blanche a %d\n", liberter);
+// 	}
+// 	if (piece == 2)
+// 	{
+// 		printf("La pierre Noir a %d\n", liberter);
+// 	}
 // }
+
+int pierre_isoler(int ** tab, int TMAX, int x, int y)
+{
+	int piece = info_case(tab, TMAX, x, y);
+	int up = info_case(tab, TMAX, x - 1, y);
+	int down = info_case(tab, TMAX, x + 1, y);
+	int right = info_case(tab, TMAX, x, y + 1);
+	int left = info_case(tab, TMAX, x, y - 1);
+	int liberter = 0;
+	if(up == 0)
+	{
+		liberter += 1;
+	}
+	if(down == 0)
+	{
+		liberter += 1;
+	}
+	if(right == 0)
+	{
+		liberter += 1;
+	}
+	if(left == 0)
+	{
+		liberter += 1;
+	}
+	
+	if (piece == 1)
+	{
+		printf("La pierre Blanche a %d\n", liberter);
+	}
+	if (piece == 2)
+	{
+		printf("La pierre Noir a %d\n", liberter);
+	}
+}
+
+void AjoutPierre(int ** tab, int x, int y, char couleur[5])
+{
+    if(couleur == "Noir"){
+        tab[x][y] = 2;
+    }
+    if(couleur == "Blanc"){
+        tab[x][y] = 1;
+    }
+}
+
 
 int main()
 {
     int TMAX = init_BoardSize();
     int ** tab = init_Board(TMAX);
 
-    // tab[2][2] = 52;
-    // printf("La valeur est de %d", tab[2][2]);
-    // Fonction juste pour tester
+    AjoutPierre(tab, 2, 2, "Blanc");
+    AjoutPierre(tab, 0, 1, "Noir");
+    
 
     display(tab, TMAX);
-    printf("Valeur est %d", info_case(tab, 0, 0));
+    pierre_isoler(tab, TMAX, 2, 2);
 
     return 0;
 }
